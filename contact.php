b3ced90264907e42c3a43cf39a331a6fa6a724e2
SBastid <78318776+SBastid@users.noreply.github.com>
2021-04-08 14:54:48 +0200

    Création Formulaire

--- contact.php
+++ contact.php
@@ -10,5 +10,23 @@
     <ul>
       <li><a href="index.html">Samuel Bastide</a></li>
     </ul>
+    <form class="formulaire" action="/contact.php" method="post">
+      <div class="">
+         <label for="Nom">Nom:</label>
+         <input type="text" name="Nom">
+      </div>
+      <div class="">
+         <label for="Prénom">Prénom:</label>
+         <input type="text" name="Prenom">
+      </div>
+      <div class="">
+         <label for="Email">Adresse Email:</label>
+         <input type="email" name="mail">
+      </div>
+      <div class="">
+         <label for="Message">Message:</label>
+         <textarea name="Message" rows="8" cols="80"></textarea>
+      </div>
+    </form>
   </body>
 </html>
